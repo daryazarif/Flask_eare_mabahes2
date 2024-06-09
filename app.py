e8aa7b1f8ac01d2f49bba98131ba71b91d448d5b
daryazarif <78638481+daryazarif@users.noreply.github.com>
2024-06-09 23:21:15 +0330

    Add files via upload

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, redirect,  url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///flask.db"
+
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	content = db.Column(db.Text, unique=True, nullable=False)
+	date = db.Column(db.DateTime, default=datetime.now)
+
+	def __repr__(self):
+		return f'Todo({self.id} - {self.content} - {self.date})'
+
+@app.route("/home")
+def home():
+	todos = Todo.query.all()
+	return render_template('home.html', todos=todos)
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+
+@app.route('/<todo_id>')
+def detail(todo_id):
+	todo = Todo.query.get(todo_id)
+	return render_template('detail.html', todo=todo)
+
+
+if __name__=="__main__":
+    app.run(debug=True)
